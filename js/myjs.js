02adf631539626902dab546b3ac31a9867453772
Alex Stepanov <stepanov22@gmail.com>
2022-05-24 22:08:01 +0200

    test n

--- js/myjs.js
+++ js/myjs.js
@@ -2,3 +2,11 @@ let area1 = document.querySelector('.area1');
 area1.onclick = function() {
     alert('work');
 }
+let area2 = document.querySelector('.area2');
+area2.onclick = function() {
+    alert('work 2');
+}
+let area3 = document.querySelector('.area3');
+area3.onclick = function() {
+    alert('work3');
+}
